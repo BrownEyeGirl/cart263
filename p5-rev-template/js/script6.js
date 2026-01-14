f6ac2948e32f49703cd158ea907027e609e5ba0b
Skyla Trousdale <skylatrousdale@gmail.com>
2026-01-14 18:47:21 -0500

    challenge 6 finished!

--- p5-rev-template/js/script6.js
+++ p5-rev-template/js/script6.js
@@ -3,3 +3,33 @@
  * Jan 14, 2026
  * 
  */
+
+
+"use strict"; 
+
+
+
+function setup() {
+    createCanvas(600, 600);
+    background(0); 
+
+}
+
+function draw() {
+
+    push(); 
+        fill(255); 
+        textSize(28); 
+        text('test', width/2, height/2);
+    pop(); 
+
+    for(let i = 0; i < 10; i++) {
+        fill(255); 
+        text(i, i*30+30, 30); 
+    }
+
+    for(let j = 0; j <= 15; j++) {
+        fill(255); 
+        text(j, 30, j*30+30); 
+    }
+}
