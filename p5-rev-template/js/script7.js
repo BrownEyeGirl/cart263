5a7ba4d1a966531200b7346b1e91b64c626f96de
Skyla Trousdale <skylatrousdale@gmail.com>
2026-01-14 19:13:12 -0500

    challenge 7 done

--- p5-rev-template/js/script7.js
+++ p5-rev-template/js/script7.js
@@ -3,3 +3,72 @@
  * Jan 14, 2026
  * 
  */
+
+"use strict"; 
+
+// circle design 
+let radius; 
+let margin; 
+
+let circles; 
+
+let r, g, b; 
+
+// slider for size 
+let slider; 
+
+function setup() {
+    createCanvas(600, 600);
+
+    slider = createSlider(10, 100);
+    slider.position(10, 10);
+    slider.size(80);
+
+
+    radius = slider.value(); 
+    margin=width%radius; // makes sure the dots are centred 
+    circles = false; // square every other line
+    //margin = 5; 
+    generateRandomColor(); 
+     background(0); 
+
+     
+}
+
+
+function draw() {
+    background(0);
+    radius = slider.value(); 
+
+    for(let x = radius+margin; x < width; x+=radius+margin) {
+        for(let y = radius+margin; y < height; y+=radius+margin) {
+            fill(r, g, b);
+            if(circles) { 
+                ellipse(x, y, radius);
+                circles = false; 
+            } 
+            else {
+                //rect(x-radius/2, y-radius/2, radius/2); 
+                rect(x-radius/2, y-radius/2, radius);
+                circles = true; 
+            }
+        }
+    }
+    circles = false; 
+}
+
+function keyPressed() {
+    if(keyCode === 32) {
+       // generateRandomColor(); 
+        r = random(0, 255);
+        g = random(0, 255); 
+        b = random(0, 255);
+        circles = !circles 
+    }
+}
+
+function generateRandomColor() {
+    r = random(0, 255);
+    g = random(0, 255); 
+    b = random(0, 255); 
+}
