cffabd42ed40f7ecb7510bb9d7c24d2c7a2a726a
Skyla Trousdale <skylatrousdale@gmail.com>
2026-03-25 19:35:48 -0400

    new meshes added

--- three-js/main.js
+++ three-js/main.js
@@ -21,28 +21,29 @@ const scene = new THREE.Scene() // creates an object Scene in the library THREE
 //A: the geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1) // 3d coordinates, vague units ? 
 //B: the material
-const material = new THREE.MeshBasicMaterial({ color: 0x800080 }) // color can also b hex or rgb 
+//const material = new THREE.MeshBasicMaterial({ color: 0x800080 }) // color can also b hex or rgb 
 //C: put together
-const mesh = new THREE.Mesh(geometry, material) // makes a mesh of combined material and geometry 
-//D: ADD TO THE SCENE
-scene.add(mesh) // 
 
-// Mesh 2
-const mesh_2 = new THREE.Mesh(geometry, material) // another box with the same mesh 
-scene.add(mesh_2)
-mesh_2.position.x = 1.5
-mesh_2.position.y = 1.25
-mesh_2.position.z = -1
+const material = new THREE.MeshBasicMaterial()
 
-// scaling 
-mesh.scale.x = 2
-mesh.scale.y = 0.25 // scaled from 1 (default
-mesh.scale.z = 0.5
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = - 1.5
 
-// rotations 
-mesh.rotation.x = Math.PI * 0.25 // order matters, in radians, also see quaternions (https://threejs.org/docs/#Quaternion) to fix rotational issues 
-mesh.rotation.y = Math.PI * 0.25
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1),
+    material
+)
 
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.5, 0.3, 16, 32),
+    material
+)
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus)
 
 
 //TURN ON AXES HELPER
@@ -69,7 +70,7 @@ scene.add(camera)
 // rotate (to look at a dif object)
   camera.lookAt(new THREE.Vector3(0, - 1, 0))
   //or
-  camera.lookAt(mesh_2.position) // look at vector of mesh 2 positon vs camera 
+  //camera.lookAt(mesh_2.position) // look at vector of mesh 2 positon vs camera 
 
 
 // 4. RENDERER 
